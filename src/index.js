6131e0bc071172f7c8bebfdf4593c3c3040c234c
Slava Semushin <slava.semushin@gmail.com>
2020-07-06 23:25:31 +0100

    chore: implement parsing of YAML config

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Generate code');
+const yaml = require('js-yaml');
+const fs = require('fs');
+
+const file = 'endpoints.yaml';
+console.log('Read', file);
+
+try {
+
+    const content = fs.readFileSync(file, 'utf8');
+    const config = yaml.safeLoad(content);
+    //console.debug(config);
+    console.log('Generate code');
+    for (let endpoint of config) {
+        console.log('GET', endpoint.path, '=>', endpoint.get);
+    }
+
+} catch (ex) {
+    console.error('Failed to parse', file);
+}
