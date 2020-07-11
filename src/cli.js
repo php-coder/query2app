2586d205a86c9ee68838896eb0938a0d8a3875f0
Slava Semushin <slava.semushin@gmail.com>
2020-07-11 13:01:08 +0100

    chore: generate simple app.js

--- src/cli.js
+++ src/cli.js
@@ -4,6 +4,8 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 
 const endpointsFile = 'endpoints.yaml';
+const resultFile = 'app.js';
+
 console.log('Read', endpointsFile);
 
 try {
@@ -11,10 +13,25 @@ try {
     const content = fs.readFileSync(endpointsFile, 'utf8');
     const config = yaml.safeLoad(content);
     //console.debug(config);
-    console.log('Generate code');
+    console.log('Generate', resultFile);
     for (let endpoint of config) {
         console.log('GET', endpoint.path, '=>', endpoint.get);
     }
+
+    const resultedCode = `const express = require('express')
+    const app = express()
+    app.set('x-powered-by', false)
+
+    app.get('${config[0].path}', (req, res) => {
+        console.debug('Execute ${config[0].get}')
+        res.sendStatus(200)
+    })
+
+    app.listen(3000, () => {
+        console.log('Listen on 3000')
+    })\n`.replace(/^    /gm, '');
+
+    fs.writeFileSync(resultFile, resultedCode);
 
 } catch (ex) {
     console.error('Failed to parse', endpointsFile);
