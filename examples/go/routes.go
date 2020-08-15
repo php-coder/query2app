a6fde355983b73e93115545500c0e5501ea94023
Slava Semushin <slava.semushin@gmail.com>
2020-08-15 13:25:27 +0100

    chore(golang): add stubs in order to pass the tests without database
    Part of #9

--- examples/go/routes.go
+++ examples/go/routes.go
@@ -1,35 +1,79 @@
 package main
 
+import "encoding/json"
+import "fmt"
 import "net/http"
+import "strconv"
 import "github.com/go-chi/chi"
 
+type Category struct {
+	Id     int     `json:"id"`
+	Name   *string `json:"name"`
+	NameRu *string `json:"name_ru"`
+	Slug   *string `json:"slug"`
+}
+
 func registerRoutes(r chi.Router) {
+	categories := make(map[int]Category)
+	cnt := 0
 
-	r.Get("/v1/categories/count", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("TODO"))
+	r.Get("/v1/categories/count", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		fmt.Fprintf(w, `{"counter": %d}`, len(categories))
+
 	})
 
-	r.Get("/v1/collections/{collectionId}/categories/count", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("TODO"))
+	r.Get("/v1/collections/{collectionId}/categories/count", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "categoryId"))
+		category, exist := categories[id]
+		if !exist {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(&category)
+
 	})
 
-	r.Get("/v1/categories", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("TODO"))
+	r.Get("/v1/categories", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		list := []Category{categories[1]}
+		json.NewEncoder(w).Encode(&list)
+
 	})
 
-	r.Post("/v1/categories", func(w http.ResponseWriter, _ *http.Request) {
+	r.Post("/v1/categories", func(w http.ResponseWriter, r *http.Request) {
+		var category Category
+		json.NewDecoder(r.Body).Decode(&category)
+		cnt += 1
+		category.Id = cnt
+		categories[cnt] = category
 		w.WriteHeader(http.StatusNoContent)
 	})
 
-	r.Get("/v1/categories/{categoryId}", func(w http.ResponseWriter, _ *http.Request) {
-		w.Write([]byte("TODO"))
+	r.Get("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "categoryId"))
+		category, exist := categories[id]
+		if !exist {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(&category)
+
 	})
 
-	r.Put("/v1/categories/{categoryId}", func(w http.ResponseWriter, _ *http.Request) {
+	r.Put("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "categoryId"))
+		var category Category
+		json.NewDecoder(r.Body).Decode(&category)
+		categories[id] = category
 		w.WriteHeader(http.StatusNoContent)
 	})
 
-	r.Delete("/v1/categories/{categoryId}", func(w http.ResponseWriter, _ *http.Request) {
+	r.Delete("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
+		id, _ := strconv.Atoi(chi.URLParam(r, "categoryId"))
+		delete(categories, id)
 		w.WriteHeader(http.StatusNoContent)
 	})
 
