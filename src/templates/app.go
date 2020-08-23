425a720b24af68544334d6f259d8c0d7db58f174
Slava Semushin <slava.semushin@gmail.com>
2020-08-23 18:23:07 +0100

    chore(golang): add code for making a database connection
    Part of #9

--- src/templates/app.go
+++ src/templates/app.go
@@ -1,11 +1,43 @@
 package main
 
+import "database/sql"
 import "fmt"
 import "net/http"
 import "os"
 import "github.com/go-chi/chi"
 
+import _ "github.com/go-sql-driver/mysql"
+
 func main() {
+	mapper := func(name string) string {
+		value := os.Getenv(name)
+		switch name {
+		case "DB_HOST":
+			if value == "" {
+				value = "localhost"
+			}
+		case "DB_NAME", "DB_USER", "DB_PASSWORD":
+			if value == "" {
+				fmt.Fprintf(os.Stderr, "%s env variable is not set or empty\n", name)
+				os.Exit(1)
+			}
+		}
+		return value
+	}
+
+	dsn := os.Expand("${DB_USER}:${DB_PASSWORD}@tcp(${DB_HOST}:3306)/${DB_NAME}", mapper)
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "sql.Open failed: %v\n", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	if err = db.Ping(); err != nil {
+		fmt.Fprintf(os.Stderr, "Could not connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
 	r := chi.NewRouter()
 	registerRoutes(r)
 
@@ -15,7 +47,7 @@ func main() {
 	}
 
 	fmt.Println("Listen on " + port)
-	err := http.ListenAndServe(":"+port, r)
+	err = http.ListenAndServe(":"+port, r)
 	fmt.Fprintf(os.Stderr, "ListenAndServe failed: %v\n", err)
 	os.Exit(1)
 }
--- src/templates/go.mod.ejs
+++ src/templates/go.mod.ejs
@@ -2,4 +2,7 @@ module main
 
 go 1.14
 
-require github.com/go-chi/chi v4.1.2+incompatible
+require (
+    github.com/go-chi/chi v4.1.2+incompatible
+    github.com/go-sql-driver/mysql v1.5.0
+)
