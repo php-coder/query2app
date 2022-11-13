5d379d25518872986b70a0308650568bc3dd6565
Slava Semushin <slava.semushin@gmail.com>
2022-11-13 16:11:31 +0700

    chore: commit forgotten auto-generated files
    Correction for 624ba06000b3eef607da7e9f7b249e43a3c07c60 commit.
    
    Relate to #27

--- examples/python/app.py
+++ examples/python/app.py
@@ -1,10 +1,10 @@
 from fastapi import FastAPI
 from routes import router
 
-from custom_routes import router as custom_route
+from custom_routes import router as custom_router
 
 app = FastAPI()
 
 app.include_router(router)
 
-app.include_router(custom_route)
+app.include_router(custom_router)
