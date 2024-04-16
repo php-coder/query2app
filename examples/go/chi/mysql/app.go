e817693104436dcb63491597a617477b5e384f7d
Slava Semushin <slava.semushin@gmail.com>
2024-04-16 08:05:49 +0700

    refactor(golang): use tabs instead of spaces for indentation

--- examples/go/chi/mysql/app.go
+++ examples/go/chi/mysql/app.go
@@ -9,45 +9,45 @@ import "github.com/jmoiron/sqlx"
 import _ "github.com/go-sql-driver/mysql"
 
 func main() {
-	mapper := func(name string) string {
-		value := os.Getenv(name)
-		switch name {
-		case "DB_HOST":
-			if value == "" {
-				value = "localhost"
-			}
-		case "DB_NAME", "DB_USER", "DB_PASSWORD":
-			if value == "" {
-				fmt.Fprintf(os.Stderr, "%s env variable is not set or empty\n", name)
-				os.Exit(1)
-			}
-		}
-		return value
-	}
+    mapper := func(name string) string {
+        value := os.Getenv(name)
+        switch name {
+        case "DB_HOST":
+            if value == "" {
+                value = "localhost"
+            }
+        case "DB_NAME", "DB_USER", "DB_PASSWORD":
+            if value == "" {
+                fmt.Fprintf(os.Stderr, "%s env variable is not set or empty\n", name)
+                os.Exit(1)
+            }
+        }
+        return value
+    }
 
-	dsn := os.Expand("${DB_USER}:${DB_PASSWORD}@tcp(${DB_HOST}:3306)/${DB_NAME}", mapper)
-	db, err := sqlx.Open("mysql", dsn)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "sqlx.Open failed: %v\n", err)
-		os.Exit(1)
-	}
-	defer db.Close()
+    dsn := os.Expand("${DB_USER}:${DB_PASSWORD}@tcp(${DB_HOST}:3306)/${DB_NAME}", mapper)
+    db, err := sqlx.Open("mysql", dsn)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "sqlx.Open failed: %v\n", err)
+        os.Exit(1)
+    }
+    defer db.Close()
 
-	if err = db.Ping(); err != nil {
-		fmt.Fprintf(os.Stderr, "Ping failed: could not connect to database: %v\n", err)
-		os.Exit(1)
-	}
+    if err = db.Ping(); err != nil {
+        fmt.Fprintf(os.Stderr, "Ping failed: could not connect to database: %v\n", err)
+        os.Exit(1)
+    }
 
-	r := chi.NewRouter()
-	registerRoutes(r, db)
+    r := chi.NewRouter()
+    registerRoutes(r, db)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "3000"
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "3000"
+    }
 
-	fmt.Println("Listen on " + port)
-	err = http.ListenAndServe(":"+port, r)
-	fmt.Fprintf(os.Stderr, "ListenAndServe failed: %v\n", err)
-	os.Exit(1)
+    fmt.Println("Listen on " + port)
+    err = http.ListenAndServe(":"+port, r)
+    fmt.Fprintf(os.Stderr, "ListenAndServe failed: %v\n", err)
+    os.Exit(1)
 }
--- examples/go/chi/mysql/routes.go
+++ examples/go/chi/mysql/routes.go
@@ -31,190 +31,190 @@ type CreateCategoryDto struct {
 }
 
 func parseBoolean(value string) bool {
-	boolValue, err := strconv.ParseBool(value)
-	if err != nil {
-		boolValue = false
-	}
-	return boolValue
+    boolValue, err := strconv.ParseBool(value)
+    if err != nil {
+        boolValue = false
+    }
+    return boolValue
 }
 
 func registerRoutes(r chi.Router, db *sqlx.DB) {
 
-	r.Get("/v1/categories/count", func(w http.ResponseWriter, r *http.Request) {
-		var result CounterDto
-		err := db.Get(&result, "SELECT COUNT(*) AS counter FROM categories")
-		switch err {
-		case sql.ErrNoRows:
-			w.WriteHeader(http.StatusNotFound)
-		case nil:
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(&result)
-		default:
-			fmt.Fprintf(os.Stderr, "Get failed: %v\n", err)
-			internalServerError(w)
-		}
-	})
+    r.Get("/v1/categories/count", func(w http.ResponseWriter, r *http.Request) {
+        var result CounterDto
+        err := db.Get(&result, "SELECT COUNT(*) AS counter FROM categories")
+        switch err {
+        case sql.ErrNoRows:
+            w.WriteHeader(http.StatusNotFound)
+        case nil:
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(&result)
+        default:
+            fmt.Fprintf(os.Stderr, "Get failed: %v\n", err)
+            internalServerError(w)
+        }
+    })
 
-	r.Get("/v1/collections/{collectionId}/categories/count", func(w http.ResponseWriter, r *http.Request) {
-		stmt, err := db.PrepareNamed("SELECT COUNT(DISTINCT s.category_id) AS counter FROM collections_series cs JOIN series s ON s.id = cs.series_id WHERE cs.collection_id = :collectionId")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "PrepareNamed failed: %v\n", err)
-			internalServerError(w)
-			return
-		}
+    r.Get("/v1/collections/{collectionId}/categories/count", func(w http.ResponseWriter, r *http.Request) {
+        stmt, err := db.PrepareNamed("SELECT COUNT(DISTINCT s.category_id) AS counter FROM collections_series cs JOIN series s ON s.id = cs.series_id WHERE cs.collection_id = :collectionId")
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "PrepareNamed failed: %v\n", err)
+            internalServerError(w)
+            return
+        }
 
-		var result CounterDto
-		args := map[string]interface{}{
-			"collectionId": chi.URLParam(r, "collectionId"),
-		}
-		err = stmt.Get(&result, args)
-		switch err {
-		case sql.ErrNoRows:
-			w.WriteHeader(http.StatusNotFound)
-		case nil:
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(&result)
-		default:
-			fmt.Fprintf(os.Stderr, "Get failed: %v\n", err)
-			internalServerError(w)
-		}
-	})
+        var result CounterDto
+        args := map[string]interface{}{
+            "collectionId": chi.URLParam(r, "collectionId"),
+        }
+        err = stmt.Get(&result, args)
+        switch err {
+        case sql.ErrNoRows:
+            w.WriteHeader(http.StatusNotFound)
+        case nil:
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(&result)
+        default:
+            fmt.Fprintf(os.Stderr, "Get failed: %v\n", err)
+            internalServerError(w)
+        }
+    })
 
-	r.Get("/v1/categories", func(w http.ResponseWriter, r *http.Request) {
-		result := []CategoryDto{}
-		err := db.Select(&result, "SELECT id , name , name_ru , slug , hidden FROM categories")
-		switch err {
-		case sql.ErrNoRows:
-			w.WriteHeader(http.StatusNotFound)
-		case nil:
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(&result)
-		default:
-			fmt.Fprintf(os.Stderr, "Select failed: %v\n", err)
-			internalServerError(w)
-		}
-	})
+    r.Get("/v1/categories", func(w http.ResponseWriter, r *http.Request) {
+        result := []CategoryDto{}
+        err := db.Select(&result, "SELECT id , name , name_ru , slug , hidden FROM categories")
+        switch err {
+        case sql.ErrNoRows:
+            w.WriteHeader(http.StatusNotFound)
+        case nil:
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(&result)
+        default:
+            fmt.Fprintf(os.Stderr, "Select failed: %v\n", err)
+            internalServerError(w)
+        }
+    })
 
-	r.Post("/v1/categories", func(w http.ResponseWriter, r *http.Request) {
-		var body CreateCategoryDto
-		json.NewDecoder(r.Body).Decode(&body)
+    r.Post("/v1/categories", func(w http.ResponseWriter, r *http.Request) {
+        var body CreateCategoryDto
+        json.NewDecoder(r.Body).Decode(&body)
 
-		args := map[string]interface{}{
-			"name":    body.Name,
+        args := map[string]interface{}{
+            "name":    body.Name,
 			"name_ru": body.NameRu,
 			"slug":    body.Slug,
 			"hidden":  body.Hidden,
 			"user_id": body.UserId,
-		}
-		_, err := db.NamedExec(
-			"INSERT INTO categories ( name , name_ru , slug , hidden , created_at , created_by , updated_at , updated_by ) VALUES ( :name , :name_ru , :slug , :hidden , NOW() , :user_id , NOW() , :user_id )",
-			args,
-		)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "NamedExec failed: %v\n", err)
-			internalServerError(w)
-			return
-		}
+        }
+        _, err := db.NamedExec(
+            "INSERT INTO categories ( name , name_ru , slug , hidden , created_at , created_by , updated_at , updated_by ) VALUES ( :name , :name_ru , :slug , :hidden , NOW() , :user_id , NOW() , :user_id )",
+            args,
+        )
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "NamedExec failed: %v\n", err)
+            internalServerError(w)
+            return
+        }
 
-		w.WriteHeader(http.StatusNoContent)
-	})
+        w.WriteHeader(http.StatusNoContent)
+    })
 
-	r.Get("/v1/categories/search", func(w http.ResponseWriter, r *http.Request) {
-		stmt, err := db.PrepareNamed("SELECT id , name , name_ru , slug , hidden FROM categories WHERE hidden = :hidden")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "PrepareNamed failed: %v\n", err)
-			internalServerError(w)
-			return
-		}
+    r.Get("/v1/categories/search", func(w http.ResponseWriter, r *http.Request) {
+        stmt, err := db.PrepareNamed("SELECT id , name , name_ru , slug , hidden FROM categories WHERE hidden = :hidden")
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "PrepareNamed failed: %v\n", err)
+            internalServerError(w)
+            return
+        }
 
-		result := []CategoryDto{}
-		args := map[string]interface{}{
-			"hidden": parseBoolean(r.URL.Query().Get("hidden")),
-		}
-		err = stmt.Select(&result, args)
-		switch err {
-		case sql.ErrNoRows:
-			w.WriteHeader(http.StatusNotFound)
-		case nil:
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(&result)
-		default:
-			fmt.Fprintf(os.Stderr, "Select failed: %v\n", err)
-			internalServerError(w)
-		}
-	})
+        result := []CategoryDto{}
+        args := map[string]interface{}{
+            "hidden": parseBoolean(r.URL.Query().Get("hidden")),
+        }
+        err = stmt.Select(&result, args)
+        switch err {
+        case sql.ErrNoRows:
+            w.WriteHeader(http.StatusNotFound)
+        case nil:
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(&result)
+        default:
+            fmt.Fprintf(os.Stderr, "Select failed: %v\n", err)
+            internalServerError(w)
+        }
+    })
 
-	r.Get("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
-		stmt, err := db.PrepareNamed("SELECT id , name , name_ru , slug , hidden FROM categories WHERE id = :categoryId")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "PrepareNamed failed: %v\n", err)
-			internalServerError(w)
-			return
-		}
+    r.Get("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
+        stmt, err := db.PrepareNamed("SELECT id , name , name_ru , slug , hidden FROM categories WHERE id = :categoryId")
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "PrepareNamed failed: %v\n", err)
+            internalServerError(w)
+            return
+        }
 
-		var result CategoryDto
-		args := map[string]interface{}{
-			"categoryId": chi.URLParam(r, "categoryId"),
-		}
-		err = stmt.Get(&result, args)
-		switch err {
-		case sql.ErrNoRows:
-			w.WriteHeader(http.StatusNotFound)
-		case nil:
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(&result)
-		default:
-			fmt.Fprintf(os.Stderr, "Get failed: %v\n", err)
-			internalServerError(w)
-		}
-	})
+        var result CategoryDto
+        args := map[string]interface{}{
+            "categoryId": chi.URLParam(r, "categoryId"),
+        }
+        err = stmt.Get(&result, args)
+        switch err {
+        case sql.ErrNoRows:
+            w.WriteHeader(http.StatusNotFound)
+        case nil:
+            w.Header().Set("Content-Type", "application/json")
+            json.NewEncoder(w).Encode(&result)
+        default:
+            fmt.Fprintf(os.Stderr, "Get failed: %v\n", err)
+            internalServerError(w)
+        }
+    })
 
-	r.Put("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
-		var body CreateCategoryDto
-		json.NewDecoder(r.Body).Decode(&body)
+    r.Put("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
+        var body CreateCategoryDto
+        json.NewDecoder(r.Body).Decode(&body)
 
-		args := map[string]interface{}{
-			"name":       body.Name,
+        args := map[string]interface{}{
+            "name":       body.Name,
 			"name_ru":    body.NameRu,
 			"slug":       body.Slug,
 			"hidden":     body.Hidden,
 			"user_id":    body.UserId,
 			"categoryId": chi.URLParam(r, "categoryId"),
-		}
-		_, err := db.NamedExec(
-			"UPDATE categories SET name = :name , name_ru = :name_ru , slug = :slug , hidden = :hidden , updated_at = NOW() , updated_by = :user_id WHERE id = :categoryId",
-			args,
-		)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "NamedExec failed: %v\n", err)
-			internalServerError(w)
-			return
-		}
+        }
+        _, err := db.NamedExec(
+            "UPDATE categories SET name = :name , name_ru = :name_ru , slug = :slug , hidden = :hidden , updated_at = NOW() , updated_by = :user_id WHERE id = :categoryId",
+            args,
+        )
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "NamedExec failed: %v\n", err)
+            internalServerError(w)
+            return
+        }
 
-		w.WriteHeader(http.StatusNoContent)
-	})
+        w.WriteHeader(http.StatusNoContent)
+    })
 
-	r.Delete("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
-		args := map[string]interface{}{
-			"categoryId": chi.URLParam(r, "categoryId"),
-		}
-		_, err := db.NamedExec(
-			"DELETE FROM categories WHERE id = :categoryId",
-			args,
-		)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "NamedExec failed: %v\n", err)
-			internalServerError(w)
-			return
-		}
+    r.Delete("/v1/categories/{categoryId}", func(w http.ResponseWriter, r *http.Request) {
+        args := map[string]interface{}{
+            "categoryId": chi.URLParam(r, "categoryId"),
+        }
+        _, err := db.NamedExec(
+            "DELETE FROM categories WHERE id = :categoryId",
+            args,
+        )
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "NamedExec failed: %v\n", err)
+            internalServerError(w)
+            return
+        }
 
-		w.WriteHeader(http.StatusNoContent)
-	})
+        w.WriteHeader(http.StatusNoContent)
+    })
 
 }
 
 func internalServerError(w http.ResponseWriter) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusInternalServerError)
-	io.WriteString(w, `{"error":"Internal Server Error"}`)
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusInternalServerError)
+    io.WriteString(w, `{"error":"Internal Server Error"}`)
 }
