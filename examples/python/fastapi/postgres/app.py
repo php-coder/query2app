9cd131830a0ed36f7092b458ed9d3da2466c9af0
Slava Semushin <slava.semushin@gmail.com>
2024-04-22 09:09:07 +0700

    feat(python): return JSON response with Internal Server Error for any exceptions
    Part of #48

--- examples/python/fastapi/postgres/app.py
+++ examples/python/fastapi/postgres/app.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
 from routes import router
 
 from custom_routes import router as custom_router
 
 app = FastAPI()
+
+@app.exception_handler(Exception)
+async def exception_handler(request: Request, ex: Exception):
+    return JSONResponse(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        content={"error": "Internal Server Error"}
+    )
 
 app.include_router(router)
 
--- examples/python/fastapi/postgres/custom_routes.py
+++ examples/python/fastapi/postgres/custom_routes.py
@@ -6,3 +6,7 @@ router = APIRouter()
 @router.get('/custom/route')
 def customRoute():
     return { "custom": True }
+
+@router.get('/custom/exception')
+def customException():
+    raise RuntimeError('expected error')
