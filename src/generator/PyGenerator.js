a74adb5479de3dc7b937fa121da17dff230dd633
Slava Semushin <slava.semushin@gmail.com>
2024-04-30 07:33:04 +0700

    chore(python): add --port option to the instruction
    Should be in 2fc11145a4ecebd115f9a91d87eae602b6606052 commit.
    
    Relate to #16

--- src/generator/PyGenerator.js
+++ src/generator/PyGenerator.js
@@ -5,7 +5,7 @@ module.exports = class PyGenerator {
   pip install -r requirements.txt
 to install its dependencies and
   export DB_NAME=db DB_USER=user DB_PASSWORD=secret
-  uvicorn app:app
+  uvicorn app:app --port 3000
 afteward to run`
     }
 
